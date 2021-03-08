c4a2ecdbea95156765a73e2a8daa23b55d2caddb
David <david.lobry96@gmail.com>
2021-03-08 21:46:30 +0100

    nav started + react router dom installed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,52 +1,22 @@
-import React from 'react';
-import { Layout, Menu } from 'antd';
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import React from 'react'
 
-const Navbar = () => {
-
-  const { Header, Content, Footer, Sider } = Layout;
-
-  return (
-    <div>
-        <Layout>
-    <Sider
-      breakpoint="lg"
-      collapsedWidth="0"
-      onBreakpoint={broken => {
-        console.log(broken);
-      }}
-      onCollapse={(collapsed, type) => {
-        console.log(collapsed, type);
-      }}
-    >
-      <div className="logo" />
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1" icon={<UserOutlined />}>
-          nav 1
-        </Menu.Item>
-        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-          nav 2
-        </Menu.Item>
-        <Menu.Item key="3" icon={<UploadOutlined />}>
-          nav 3
-        </Menu.Item>
-        <Menu.Item key="4" icon={<UserOutlined />}>
-          nav 4
-        </Menu.Item>
-      </Menu>
-    </Sider>
-    <Layout>
-      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
-      <Content style={{ margin: '24px 16px 0' }}>
-        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-          content
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-    </Layout>
-  </Layout>
-  </div>
+const Navbar = () => (
+    <nav className="navbar">
+      <ul>
+        <li>
+         Home
+        </li> 
+        <li>
+         Register
+        </li> 
+        <li>
+         Login
+        </li> 
+        <li>
+         Logout
+        </li> 
+      </ul>
+    </nav>
   )
-  }
 
 export default Navbar
