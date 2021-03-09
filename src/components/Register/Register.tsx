d0ca8ffd7fd1ca37e0adbad3ea8bdef1de2535f2
Uriel <uriel.sautron@gmail.com>
2021-03-09 14:42:54 +0100

    Create form register

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,7 +1,100 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from 'react';
+import { Form, Input, Button, Select } from 'antd';
 
-const Register = () => (
-    <div>Hi it's register</div>
-  )
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
 
-export default Register
+const Register = () => {
+  interface OnFinishValue {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    passwordConfirmation: string;
+    role: string;
+  }
+
+  const onFinish = (values: OnFinishValue) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Firstname"
+        name="firstName"
+        rules={[{ required: true, message: 'Please input your firstname!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Lastname"
+        name="lastName"
+        rules={[{ required: true, message: 'Please input your lastname!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Email"
+        name="email"
+        rules={[{ required: true, type: 'email', message: 'Please input your email!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Role"
+        name="role"
+        rules={[{ required: true, message: 'Please choose your role!' }]}
+      >
+        <Select>
+          <Select.Option value="student">Student</Select.Option>
+          <Select.Option value="teacher">Teacher</Select.Option>
+          <Select.Option value="admin">Admin</Select.Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        label="Password Confirmation"
+        name="passwordConfirmation"
+        rules={[{ required: true, message: 'Please input your password confirmation!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Register;
