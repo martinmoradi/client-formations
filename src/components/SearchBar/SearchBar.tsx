03f43b306264d81b8ae12869e8f2d4407bb00e35
David <david.lobry96@gmail.com>
2021-03-10 19:01:24 +0100

    categories btn added

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Input, Space } from 'antd';
-import { AudioOutlined } from '@ant-design/icons';
+import { Input, Space, Menu, Dropdown, Button, message } from 'antd';
+import { AudioOutlined, DownOutlined, UserOutlined } from '@ant-design/icons';
+import { MenuInfo } from 'rc-menu/lib/interface';
 
 const SearchBar = () => {
   const { Search } = Input;
@@ -14,9 +15,30 @@ const SearchBar = () => {
     />
   );
   
+  const menu = (
+    <Menu>
+      <Menu.Item key="1" icon={<UserOutlined />}>
+        1st menu item
+      </Menu.Item>
+      <Menu.Item key="2" icon={<UserOutlined />}>
+        2nd menu item
+      </Menu.Item>
+      <Menu.Item key="3" icon={<UserOutlined />}>
+        3rd menu item
+      </Menu.Item>
+    </Menu>
+  );
+
   const onSearch = (value: string) => console.log(value);
   return (
-    <div className="form-container">
+    <div className="searchbar-container">
+      <div className="searchbar-categories">
+    <Dropdown overlay={menu}>
+      <Button>
+        Categories <DownOutlined />
+      </Button>
+    </Dropdown>
+      </div>
       <Space direction="vertical">
         <Search placeholder="What do you want to learn ?" onSearch={onSearch} enterButton size="large" style={{ width: 400 }} />
       </Space>
