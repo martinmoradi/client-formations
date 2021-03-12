5b43e09aaaffb76b41439705d99891d6e975df6a
Uriel <uriel.sautron@gmail.com>
2021-03-12 01:32:06 +0100

    AdminSession Delete session

--- src/pages/Admin/AdminSessions/AdminSessions.tsx
+++ src/pages/Admin/AdminSessions/AdminSessions.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import SessionList from '../../../components/SessionList/SessionList';
+import ModalSession from '../../../components/Modal/ModalSession';
 
 const AdminSessions = () => (
   <div>
     <h2>Admin Sessions</h2>
+    <ModalSession />
     <SessionList />
   </div>
 );
